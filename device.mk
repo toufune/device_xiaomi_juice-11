7cc40cfb00990e6facaf8308e00b2f6e30200fea
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-21 10:33:34 +0000

    juice: Build servicetracker@1.2 vendor variant
    *F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.servicetracker@1.2-service": library "vendor.qti.hardware.servicetracker@1.2.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -413,6 +413,14 @@ PRODUCT_PACKAGES += \
     fstab.emmc \
     ueventd.qcom.rc
 
+# Service Tracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-service \
+    vendor.qti.hardware.servicetracker@1.2-impl \
+    vendor.qti.hardware.servicetracker@1.0.vendor \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor \
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
