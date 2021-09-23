aa7c531a698cb54abfabf6405223eee926f4f44a
Inok <inok.dr189@gmail.com>
2021-09-23 08:12:30 +0700

    juice: Initial Juice Device Tree
    * Allow broken duplicate build rules
    * Set Device Path
    * Set Board Vendor Name
    * Set Flag Architecture
    * Set Board Platform CPU and GPU
    * Set Booloader Name
    * Set Board Ant Wireless
    * Set Assert Device
    * Set Lib Init Juice
    * Set Shipping API Level To 29
    * Set VNDK Version To 30
    * Use 4GB dalvik-heap config
    * Enabled updateable APEX
    * Adding GSI keys
    * Inherit the proprietary vendor
    * Bring-UP AOSP ROM
    
    Signed-off-by: Inok <inok.dr189@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter lemon pomelo citrus lime juice,$(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
